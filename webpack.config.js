9ea7cdf2f5e1c594a5621c9f1628f71dcbcbeaf7
Ronnie Kaczynski <ronniie.k@protonmail.com>
2024-11-28 13:03:03 +0000

    fix webpack size warnings

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,47 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
 
 module.exports = {
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
+  devtool: false,
   entry: './src/main.js',
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
     publicPath: '/'
+  },
+  optimization: {
+    usedExports: true,
+    minimize: true,
+    splitChunks: {
+      chunks: 'all',
+    }
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'index.html'),
       inject: 'body',
+      external: {
+        'pixi.js': 'https://cdn.jsdelivr.net/npm/pixi.js@8.x/dist/pixi.min.js',
+      }
     }),
     new CopyPlugin({
       patterns: [
         { from: "src/assets", to: "assets" }
       ],
+    }),
+    new CompressionWebpackPlugin({
+      algorithm: 'gzip',
+      test: /\.js$|\.html$/,
+      threshold: 1024,
+      minRatio: 0.8,
     }),
   ],
   module: {
@@ -37,5 +60,5 @@ module.exports = {
     port: 3000,
     hot: true
   },
-  mode: 'development',
+  mode: 'production',
 };
